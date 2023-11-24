ee3913dc7993523d7662985b7d1b3755d9ef544b
luke fergusson <86552861+luke-fergusson@users.noreply.github.com>
2023-11-24 10:59:38 +0000

    Added a board class

---
+++ tetris/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+public class Board : Game
+{
+	public Board()
+	{
+		
+	}
+	protected override void initalised()
+	{
+
+
+		base.initalised();
+	}
+}
